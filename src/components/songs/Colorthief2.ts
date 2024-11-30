0efc357eafe5ec2d8f23e8b8800f0b8222b0b0f5
Juan Diego Montenegro Segura <menethil0503@gmail.com>
2024-11-30 10:48:44 -0500

    Agregando doble ID container para su doble uso (ajuste pendiente para ser reutilizable)

--- src/components/songs/Colorthief2.ts
+++ src/components/songs/Colorthief2.ts
@@ -1,12 +1,15 @@
 // colorExtractor.ts
+// Mala practica! Corregirse en otro momento para que sea reutilizable
+
 import ColorThief from 'colorthief';
 
-export function setupColorExtraction(imageId: string, containerId: string) {
+export function setupColorExtraction(imageId: string, containerId: string, containerId2: string) {
   const img = document.getElementById(imageId) as HTMLImageElement;
   const container = document.getElementById(containerId);
+  const container2 = document.getElementById(containerId2);
   const colorThief = new ColorThief();
 
-  if (!img || !container) {
+  if (!img || !container || !container2) {
     console.error('Image or container element not found');
     return;
   }
@@ -18,6 +21,7 @@ export function setupColorExtraction(imageId: string, containerId: string) {
         const color1 = `rgb(${palette[0].join(',')})`;
         const color2 = `rgb(${palette[1].join(',')})`;
         container.style.background = `linear-gradient(to top, ${color1}, ${color2})`;
+        container2.style.background = `linear-gradient(to top, ${color1}, ${color2})`;
       }
     } catch (error) {
       console.error('Error extracting colors:', error);
